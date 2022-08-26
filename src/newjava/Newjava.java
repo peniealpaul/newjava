facac28870a5c7799d571f1947c70eec9c28ba83
paul <peniealpaul02@gmail.com>
2022-08-26 09:33:41 +0530

    swap two int

--- src/newjava/Newjava.java
+++ src/newjava/Newjava.java
@@ -1,12 +1,15 @@
 package newjava;
 
-public class Newjava {
+public class Newjava {public static void main(String[] args) {
+		int n1=5,n2=21,n3;
+		System.out.println("before swap");
+		System.out.println("n1="+n1);
+		System.out.println("n2="+n2);
+		n3=n1;
+		n1=n2;
+		n2=n3;
+		System.out.println("after swap");
+		System.out.println("n1="+n1);
+		System.out.println("n2="+n2);}}
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.print("hello world");
-
-	}
-
-}
  
