b93ba57c426ae0b5e6fb7cc0e5fbed74ca941ba0
paul <paul@paul>
2022-09-07 17:20:08 +0530

    try

--- src/newjava/tryy.java
+++ src/newjava/tryy.java
@@ -3,8 +3,57 @@ package newjava;
 class tryy {
 	public static void main(String[] args) {
 
-		int k =4;
-		if(true&&k++<5) {}
-			System.out.print(k);
-		}}
+		int a=11;int b=22;
+		int c=56;
+		/*if(a==b&&b==c) {
+			System.out.print("equal");}
+		/*else {
+			int c=a>b?a:b;
+			System.out.print("large num is="+c);*/
+		/*if(a<b&&c<b) {
+			System.out.print("b is big");}
+			if(c<a&&b<a) {
+				System.out.print("a is big");
+			}
+			if(b<c&&b<a) {
+				System.out.print("c is big");*/
+	
+			/*int d=a>b?a:b;
+		int n=c<d?d:c;
+		if(n==a) {
+			System.out.print("a is big= "+n);
+		}
+		if(n==b) {
+			System.out.print("b is big= "+n);}
+			if(n==c) {
+				System.out.print("c is big= "+n);
+			}}}*/
+		int i=1;
+		for(;i<=10;i++) {
+			System.out.println(i+"* 3 = "+i*3);
+		/*int k=0;
+		
+		for(int i=0;i<=5;i++) {
+			k+=i;//k=k+i
+	
+			
+		}
+		System.out.println(k);
+		
+		
+		
+		
+		
+		int y=0;
+		for(int t=0;t<=10;t+=2) {
+			y+=t;//y=y+t
+		
+			}System.out.print(y);*/
+		}
+			}}
+		
+		
+			
+		
+		
 
