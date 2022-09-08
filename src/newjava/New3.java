feed78a602175df72536ae92a6b05da733285692
paul <paul@paul>
2022-09-08 18:14:22 +0530

    0,2-4,6,-8

---
+++ src/newjava/New3.java
@@ -0,0 +1,16 @@
+package newjava;
+
+public class New3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=0;i<20;i+=2) {
+			if(i%4==0) {
+			System.out.println(-i);}
+			else {System.out.println(" "+i);
+			
+		
+		}
+	}}}
+
+
