823487be5e49e4653b48044f57222b5d2930048c
paul <paul@paul>
2022-09-08 18:14:57 +0530

    mixed pro

---
+++ src/newjava/countdig.java
@@ -0,0 +1,44 @@
+package newjava;
+
+public class countdig {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+/*int a=0;
+int b=0;
+int c=1;
+int d=0;
+while(a<10) {
+	System.out.println(b);
+	b=b+c;
+	c=d;
+	
+	a++;
+}int a=4;
+a=a+a++-(--a);
+System.out.print();*/
+		/*int j=0;
+		int i=0;
+		int k=0;
+		int y=1;
+		for( i=1;i<10;i++) {
+			k=i;
+			for( j=1;j<i;j++) {
+				y=y*k;	}
+			System.out.println(y);*/
+		for(int i=1;i<10;i++)
+		{
+		int a=i;
+		for(int j=1;j<i;j++) 
+		 {a=a*i;}
+		System.out.println(a);/*
+		for(int i=0;i<20;i+=2) {
+			if(i%4==0) {
+			System.out.println(-i);}
+			else {System.out.println(" "+i);*/
+			
+		
+		}}}
+		
+	    
+	
