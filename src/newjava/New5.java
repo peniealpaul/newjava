30c27d3c788419b028c6627629bbf05c7c4e8f3a
paul <paul@paul>
2022-09-12 08:16:38 +0530

    days

--- src/newjava/New5.java
+++ src/newjava/New5.java
@@ -2,40 +2,19 @@ package newjava;
 
 public class New5 {
 	public static void main(String arg[]) {
-		int n=7;
-		
+		int n=1;
 		switch(n) {
 		case 1:
-			System.out.print("week days");
-			break;
-			
 		case 2:
-			System.out.print("week days");
-			break;
-			
 		case 3:
-			System.out.print("week days");
-			break;
-			
 		case 4:
-			System.out.print("week days");
-			break;
-			
 		case 5:
 			System.out.print("week days");
 			break;
-			
 		case 6:
-			System.out.print("week end");
-			break;
-			
 		case 7:
 			System.out.print("week end");
-		
+		break;
 			default:
 				System.out.print("invalid");
-			
-
-	
-
-}}}
+			}}}
