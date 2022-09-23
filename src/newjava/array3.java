ddb0384339a7edb5bb541f4a4b8bf8a398bdd6ad
paul <paul@paul>
2022-09-23 20:36:06 +0530

    array

--- src/newjava/array3.java
+++ src/newjava/array3.java
@@ -4,27 +4,25 @@ public class array3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	
-		int a[]= {1,2,3,7,6};
-		for(int k=0;k<6;k++) {
-		int b=a[a.length-1];
-		
-		for(int i=a.length-1;i>0;i--) {
-			
-		a[i]=a[i-1];
-		
-		}a[0]=b;}
-		
-for(int j=0;j<=a.length-1;j++) {
-	
-	System.out.println(a[j]);
 
-}
-		/*int a[]= {1,2,3,4,5,6,7,8,12};
-		int b=a[a.length-1];
-		System.out.println(b);*/
-		
-		
+		int a[] = { 1, 2, 3, 7, 6 };
+		for (int k = 0; k < 1; k++) {
+			int b = a[a.length-1];
+
+			for (int i = a.length-1; i <0; i++) {
+
+				a[i-1] = a[i];
+
+			}
+			a[a.length-1] = b;
+		}
+
+		for (int j = 0; j <= a.length - 1; j++) {
+
+			System.out.println(a[j]);
+
+		}
+
 	}
 
 }
--- src/newjava/pattern1.java
+++ src/newjava/pattern1.java
@@ -1,17 +1,20 @@
 package newjava;
+
 import java.util.Scanner;
 
 public class pattern1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner sc=new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		System.out.println("enter number");
-		String num=sc.nextLine();
+		String num = sc.nextLine();
 		int i;
 		char j;
-     for( i=0,j='A';i<=num.length()-1;i++,j++) {
-	char u=num.charAt(i);
-	System.out.println(j+"="+u);
-	
-			}}}
+		for (i = 0, j = 'A'; i <= num.length() - 1; i++, j++) {
+			char u = num.charAt(i);
+			System.out.println(j + "=" + u);
+
+		}
+	}
+}
