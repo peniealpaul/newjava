d9e7dffde26fd55810391f3ec5d4cfd8dee9da38
paul <paul@paul>
2022-11-06 11:52:35 +0530

    interview sum

--- src/newjava/Stringformat.java
+++ src/newjava/Stringformat.java
@@ -4,7 +4,25 @@ public class Stringformat {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		String a="100000000";
+		String b="";
+		b=b+"Rs ";
+		int count=0;
+		for(int i=0;i<a.length();i++) {
+			count++;
+			b=b+a.charAt(i);
+			if(i<a.length()-3)       
+			if(a.length()%2==0) {
+				if(count%2==1) {
+				b=b+",";}
+			}else {
+				if(count%2==0) {
+					b=b+",";b
+				}
+			}
+			   }
+		b=b+".00/-";
+		System.out.println(b);
+}
 	}
 
-}
